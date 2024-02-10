abd7d62e86044c49c27a10464a9cc2fb13148ecd
DevashishShakya <devashish24122001@gmail.com>
2024-02-10 14:54:37 +0530

    4th part done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,26 +1,29 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
-import generteTokenAndSetCookie from "../utils/generateToken.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
-    if (password != confirmPassword) {
-      return res.status(400).json({ error: "password do not match" });
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
     }
+
     const user = await User.findOne({ username });
+
     if (user) {
       return res.status(400).json({ error: "Username already exists" });
     }
 
-    // HASH PASS
+    // HASH PASSWORD HERE
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // https://avatar.iran.liara.run/public/boy?username=Scott
+    // https://avatar-placeholder.iran.liara.run/
 
-    const boyProfilePic = ` https://avatar.iran.liara.run/public/boy?username=${username}`;
-    const girlProfilePic = ` https://avatar.iran.liara.run/public/girl?username=${username}`;
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const newUser = new User({
       fullName,
@@ -31,12 +34,18 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      // gen jwt
-      generteTokenAndSetCookie(newUser._id, res);
+      // Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
-      res.status(201).json(newUser);
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
     } else {
-      res.status(400).json({ error: "invalid user data" });
+      res.status(400).json({ error: "Invalid user data" });
     }
   } catch (error) {
     console.log("Error in signup controller", error.message);
@@ -48,15 +57,17 @@ export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    const isCorrectPassword = await bcrypt.compare(
+    const isPasswordCorrect = await bcrypt.compare(
       password,
       user?.password || ""
     );
 
-    if (!user || !isCorrectPassword) {
+    if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password" });
     }
-    generteTokenAndSetCookie(user._id, res);
+
+    generateTokenAndSetCookie(user._id, res);
+
     res.status(200).json({
       _id: user._id,
       fullName: user.fullName,
@@ -64,12 +75,12 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.log("Error in signup controller", error.message);
-    res.status(500).json({ error: "Invalid username or password" });
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-export const logout = async (req, res) => {
+export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,11 +1,13 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    let receiverId = req.params.id;
+    const { id: receiverId } = req.params;
     const senderId = req.user._id;
-    // console.log("Receiver ID:", req.params.id);
+
     let conversation = await Conversation.findOne({
       participants: { $all: [senderId, receiverId] },
     });
@@ -26,17 +28,22 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // Socketio Functionlity here
+    // await conversation.save();
+    // await newMessage.save();
 
-    // await conversation.save();  //1
-    // console.log("New Message:", newMessage);
-    // await newMessage.save();  //1
-
+    // this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
-    console.log("Error in sendMessage controller:", error);
+    console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -48,12 +55,15 @@ export const getMessages = async (req, res) => {
 
     const conversation = await Conversation.findOne({
       participants: { $all: [senderId, userToChatId] },
-    }).populate("messages");
+    }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
     if (!conversation) return res.status(200).json([]);
-    res.status(200).json(conversation.messages);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in getMessages controller:", error);
+    console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
